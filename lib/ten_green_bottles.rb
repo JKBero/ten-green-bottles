4dfa2cdb458785acdd0461be070d2a07b0f412ba
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-03-25 16:29:22 +0000

    feat: returns final verse once down to the final bottle

--- lib/ten_green_bottles.rb
+++ lib/ten_green_bottles.rb
@@ -3,7 +3,7 @@ require 'humanize'
 class TenGreenBottles
 
   def lyrics(bottles = 10)
-    verse(bottles)
+    bottles == 1 ? final_verse : verse(bottles)
   end
 
   private
@@ -13,6 +13,10 @@ class TenGreenBottles
     y = (bottles-1).humanize
     plural = "s" if bottles > 2
     "#{x} green bottles\nHanging on the wall\n#{x} green bottles\nHanging on the wall\nAnd if one green bottle\nShould accidentally fall\nThere'll be #{y} green bottle#{plural}\nHanging on the wall"
+  end
+
+  def final_verse
+    "One green bottle\nHanging on the wall\nOne green bottle\nHanging on the wall\nIf that one green bottle\nShould accidentally fall\nThere'll be no green bottles\nHanging on the wall"
   end
 
 end
--- spec/ten_green_bottles_spec.rb
+++ spec/ten_green_bottles_spec.rb
@@ -24,5 +24,10 @@ describe TenGreenBottles do
       result = "Two green bottles\nHanging on the wall\nTwo green bottles\nHanging on the wall\nAnd if one green bottle\nShould accidentally fall\nThere'll be one green bottle\nHanging on the wall"
       expect(song.lyrics(2)).to eq result
     end
+
+    it 'returns the final verse to the song' do
+      result = "One green bottle\nHanging on the wall\nOne green bottle\nHanging on the wall\nIf that one green bottle\nShould accidentally fall\nThere'll be no green bottles\nHanging on the wall"
+      expect(song.lyrics(1)).to eq result
+    end
   end
 end
