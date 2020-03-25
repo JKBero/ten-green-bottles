4749cb7ccb690b70c613969b9c3ccea8d41ec5da
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-03-25 16:13:39 +0000

    feat: second verse is returned when started with 10 bottles

--- spec/ten_green_bottles_spec.rb
+++ spec/ten_green_bottles_spec.rb
@@ -1,11 +1,18 @@
 require 'ten_green_bottles'
 
 describe TenGreenBottles do
-  describe '.lyrics' do
+  describe '#lyrics' do
+
+    subject(:song) { TenGreenBottles.new }
+
     it 'returns the first verse to Ten Green Bottles' do
-      song = TenGreenBottles.new
       result = "Ten green bottles\nHanging on the wall\nTen green bottles\nHanging on the wall\nAnd if one green bottle\nShould accidentally fall\nThere'll be nine green bottles\nHanging on the wall"
       expect(song.lyrics).to eq result
+    end
+
+    it 'returns the second verse to Ten Green Bottles' do
+      result = "Nine green bottles\nHanging on the wall\nNine green bottles\nHanging on the wall\nAnd if one green bottle\nShould accidentally fall\nThere'll be eight green bottles\nHanging on the wall"
+      expect(song.lyrics(9)).to eq result
     end
   end
 end
